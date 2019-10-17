ca72637e11eeff6d405ff9aae41484f6ccddac0a
Dianna Sokhan <diannasokhan@gmail.com>
2019-10-17 19:59:21 -0400

    adding route to handle post user signup

--- spec/integration/users_spec.js
+++ spec/integration/users_spec.js
@@ -18,9 +18,53 @@ describe("routes: users", () => {
         it("should render a view with a sign up form", (done) => {
             request.get(`${base}/sign_up`, (err, res, body) => {
                 expect(err).toBeNull();
-                expect(body).toContain("Sign Up");
+                expect(body).toContain("Sign up");
                 done();
             })
         })
+    })
+    describe("POST /user/signup", () => {
+        it("should create a new user with valid values and redirect", (done) => {
+            const options = {
+                url: "http://localhost:3000/user/signup",
+                form: {
+                    email: "diannaG@gmail.com",
+                    password: "hello12",
+                    passwordConfirmation: "hello12"
+                }
+            }
+            request.post(options, (err, res, body) => {
+                User.findOne({where: {email: "diannaG@gmail.com"}})
+                .then((user) => {
+                    console.log(user)
+                    expect(user).not.toBeNull();
+                    expect(user.email).toBe("diannaG@gmail.com");
+                    expect(user.id).toBe(1);
+                    done();
+                }).catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+        it("should not create a new user with invalid attributes and redirect", () => {
+            const options = {
+                url:"http://localhost:3000/user/signup",
+                form: {
+                    email: "hihi",
+                    password: "hellohello"
+                }
+            }
+            request.post(options, (err, res, body) => {
+                User.findOne({where: {email: "hihi"}})
+                .then((user) => {
+                    expect(user).toBeNull();
+                    done();
+                }).catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
     })
 })
