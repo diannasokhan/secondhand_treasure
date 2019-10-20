6fd7cecf81553f58930c915a1d7a5374962f9e2a
Dianna Sokhan <diannasokhan@gmail.com>
2019-10-19 23:42:01 -0400

    fixing messages view

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,9 +1,9 @@
 module.exports = {
     validateUsers(req, res, next) {
         if(req.method === "POST") {
-          req.checkBody("email", "must be valid").isEmail();
-          req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
-          req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+          req.checkBody("email", "email must be valid").isEmail();
+          req.checkBody("password", "password must be at least 6 characters in length").isLength({min: 6})
+          req.checkBody("passwordConfirmation", "password confirmation must match password provided").optional().matches(req.body.password);
         }
         const errors = req.validationErrors();
    
